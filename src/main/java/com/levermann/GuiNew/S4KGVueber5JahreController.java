433f34c3ebdc6a25396384b91c5f4bbd243bc1a8
Lisa <lisa.stolz@live.de>
2019-08-08 12:25:52 +0200

    Set correct Package Name :D

--- src/main/java/com/levermann/GuiNew/S4KGVueber5JahreController.java
+++ src/main/java/com/levermann/GuiNew/S4KGVueber5JahreController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.lervermann.Gui2;
+package com.levermann.GuiNew;
 
 import java.net.URL;
 import java.util.ResourceBundle;
