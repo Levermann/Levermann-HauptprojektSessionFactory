3140660696f39ce6aabe2c8f2392e267432b4d46
Lisa <lisa.stolz@live.de>
2019-08-21 03:21:24 +0200

    test Hibernate Util + Connection Main

--- src/main/java/com/levermann/mainMethod/ConnectionMainFactory.java
+++ src/main/java/com/levermann/mainMethod/ConnectionMainFactory.java
@@ -1,8 +1,10 @@
 package com.levermann.mainMethod;
 
 import com.levermann.entityclass.Company;
-import com.levermann.sessionControlClasses.Create;
+import com.levermann.sessionControlClasses.HibernateUtil;
 import org.apache.log4j.BasicConfigurator;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 
 abstract public class ConnectionMainFactory  {
@@ -10,52 +12,35 @@ abstract public class ConnectionMainFactory  {
 
  @SuppressWarnings("empty-statement")
  public static void main(String[] args) {
+
      BasicConfigurator.configure();
 
-     Company company = new Company();
+     try {
+         Session s = HibernateUtil.getSession();
+         s.beginTransaction();
+         s.getTransaction().commit();
+         System.out.println("Insert");
 
-     //KursgewinnVerhaeltnis5Jahre kg5 = new KursgewinnVerhaeltnis5Jahre();
+     }
+     catch (Exception e) {
+         System.out.println("Erroryyy" + e.getMessage());
+     }
+  /*   final Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
 
-//     kg5.KursgewinnVerhaeltnis5Jahre();
+        Company company = new Company();
+        company.setCompanyname("BMW");
+        company.setEigenkapital(123214);
+        company.setFremdkapital(324234);
 
- //Zeigt Systeminfos auf
- // BasicConfigurator.configure();
+        session.save(company);
 
-  // Gibt die Methoden aus dem Interface aus
-
-
-
-   //  Gewinnrevision gwr = new Gewinnrevision();
-  //   gwr.Gewinnrevision();
-
-   //  KursheuteggKursvor1Jahr kw6 = new KursheuteggKursvor1Jahr();
-     //kw6.KursheuteggKursvor12Monaten();
-
-   //  Kursmomentum km = new Kursmomentum();
-    // km.Kursmomentum();
-
-     // Gewinnwachstum gww = new Gewinnwachstum();
-     // gww.Gewinnwachstum();
-
-    // Select select = new Select();
-     // select.Dao();
-
-  //   Update drm = new Update();
-
-    // drm.DaoJahresueberschuss("bmw11", 2000);
-        Create CreateAnalysisRating = new Create();
-     CreateAnalysisRating.CreateAnalysisRating("AppleAG");
-//     Create d = new Create();
-
-    // d.CreateUnternehmen( "bmw", (float) 2000.00);
-
-     ///Select de = new Select();
-   //  de.SelectUnternehmenByName("bmw");
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
 
 
+   */
+
+     }
 
  }
-
-
-
-}
--- src/main/java/com/levermann/sessionControlClasses/HibernateUtil.java
+++ src/main/java/com/levermann/sessionControlClasses/HibernateUtil.java
@@ -1,68 +1,72 @@
 package com.levermann.sessionControlClasses;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.apache.log4j.Logger;
 
 import javax.imageio.spi.ServiceRegistry;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HibernateUtil {
+   // private static final Logger log = Logger.getLogger(HibernateUtil.class);
 
+    @SuppressWarnings({"unchecked", "rawtypes", "serial"})
+    private static final List<Class<?>> classList = new ArrayList() {{
+        add(com.levermann.entityclass.Company.class);
+        add(com.levermann.entityclass.AnalysisSteps.class);
+        add(com.levermann.entityclass.AnalysisRating.class);
+    }};
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
+
         try {
             if (sessionFactory == null) {
                 Configuration configuration = new Configuration().configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
                 StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
                 serviceRegistryBuilder.applySettings(configuration.getProperties());
                 ServiceRegistry serviceRegistry = (ServiceRegistry) serviceRegistryBuilder.build();
+            //    for (Class<?> pojoClass : classList) {
+                //    log.info("Adding annotated class : " + pojoClass.getCanonicalName());
+               // }
                 sessionFactory = configuration.buildSessionFactory((org.hibernate.service.ServiceRegistry) serviceRegistry);
             }
 
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed" + ex);
             throw new ExceptionInInitializerError(ex);
-        }return sessionFactory;
+        }
+
+         return sessionFactory;
+    }
+
+    public static Session getSession()
+        throws HibernateException { return sessionFactory.openSession();
+    }
+    public static Session getCurrentSession()
+        throws HibernateException { return sessionFactory.getCurrentSession();
+
     }
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+
+
     public static void shutdown() {
         // Close caches and connection pools
         getSessionFactory().close();
 
     }
-    private static Transaction getTransaction() throws Exception{
+
+    private static Transaction getTransaction() throws Exception {
         Session s = getSessionFactory().getCurrentSession();
         Transaction transaction = s.beginTransaction();
         transaction.setTimeout(12);
         return transaction;
     }
-      /*
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            return sessionFactory;
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-    public static void shutdown() {
-        // Close caches and connection pools
-        getSessionFactory().close();
-    }
-
-  */
-    }
+}
