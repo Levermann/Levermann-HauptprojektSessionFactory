cc4aa8627c6323183446666319edbd5ae74fea4b
chlad <chladny.andreas@gmail.com>
2019-08-22 15:43:18 +0200

    Added SumofPoints.sql

--- src/main/java/com/levermann/sqlStatements/SumofPoints.sql
+++ src/main/java/com/levermann/sqlStatements/SumofPoints.sql
@@ -1,16 +1,16 @@
 SELECT sum(
-       Eigenkapitalrendite+
-       EBITMarge+
-       Eigenkapitalquote+
-       KursGewinnVerhaeltnis+
-       KursGewinnVerhaeltnisAktuell+
-       Analystenmeinungen+
-       ReaktionaufQuartalszahlen+
-       Gewinnrevision+
-       Kursverlauf6Monate+
-       Kursverlauf12Monate+
-       Kursmomentum+
-       Dreimonatsreversal+
-       Gewinnwachstum)
-FROM analysisrating
-GROUP BY ID;
+                   Eigenkapitalrendite+
+                   EBITMarge+
+                   Eigenkapitalquote+
+                   KursGewinnVerhaeltnis+
+                   KursGewinnVerhaeltnisAktuell+
+                   Analystenmeinungen+
+                   ReaktionaufQuartalszahlen+
+                   Gewinnrevision+
+                   Kursverlauf6Monate+
+                   Kursverlauf12Monate+
+                   Kursmomentum+
+                   Dreimonatsreversal+
+                   Gewinnwachstum) AS "gesamtpunkte"
+FROM analysisrating, company
+WHERE company.id = analysisrating.id
