9245366e4efaf31abac66c444463fd639d904b33
Lisa <lisa.stolz@live.de>
2019-08-28 00:25:05 +0200

    added Stylesheet

--- mavenJavaFXUI/src/main/java/com/levermann/CompanyOverviewController.java
+++ mavenJavaFXUI/src/main/java/com/levermann/CompanyOverviewController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
