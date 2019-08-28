9092411fbbbe9d19a31e1df0cf47d0fcc2480698
Lisa <lisa.stolz@live.de>
2019-08-28 17:21:25 +0200

    funktionierende Liste für userinput nur nicht die richtigen werte

--- mavenJavaFXUI/src/main/java/com/levermann/ShowUserInputManageUI.java
+++ mavenJavaFXUI/src/main/java/com/levermann/ShowUserInputManageUI.java
@@ -7,12 +7,13 @@ public class ShowUserInputManageUI {
 
 
     private final SimpleStringProperty inputFigure1;
-    private final SimpleStringProperty inputValue1;
+    private final SimpleFloatProperty inputValue1;
 
-    public ShowUserInputManageUI(String userInputFigures, String userInputValues) {
+    public ShowUserInputManageUI(String userInputFigures, Float userInputValues) {
         this.inputFigure1 = new SimpleStringProperty(userInputFigures);
-        this.inputValue1 = new SimpleStringProperty(userInputValues);
+        this.inputValue1 = new SimpleFloatProperty(userInputValues);
     }
+
 
     public String getInputFigure1() {
         return inputFigure1.get();
@@ -22,18 +23,19 @@ public class ShowUserInputManageUI {
         return inputFigure1;
     }
 
-    public String getInputValue1() {
-        return inputValue1.get();
-    }
-
-    public SimpleStringProperty inputValue1Property() {
-        return inputValue1;
-    }
     public void setInputFigure1(String inputFigure1) {
         this.inputFigure1.set(inputFigure1);
     }
 
-    public void setInputValue1(String inputValue1) {
+    public float getInputValue1() {
+        return inputValue1.get();
+    }
+
+    public SimpleFloatProperty inputValue1Property() {
+        return inputValue1;
+    }
+
+    public void setInputValue1(float inputValue1) {
         this.inputValue1.set(inputValue1);
     }
 }
